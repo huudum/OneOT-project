916d146db1e42c5338403e80708b26a68eaafc97
huudum@ttu.ee <huudum@ttu.ee>
2021-12-30 15:56:25 +0200

    Base classes of Spring boot project.

---
+++ src/main/java/com/OneOT/OneOtProjectApplication.java
@@ -0,0 +1,12 @@
+package com.OneOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneOtProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneOtProjectApplication.class, args);
+    }
+}
